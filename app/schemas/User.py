2626b50e3af21d5be559c1efe31d409614196123
Sujoyi0001 <sujoygarai89@gmail.com>
2026-09-07 23:29:14 +0530

    Refactor user management code for improved structure and functionality

--- app/schemas/User.py
+++ app/schemas/User.py
@@ -2,16 +2,17 @@ from pydantic import BaseModel, Field
 
 class UserCreate(BaseModel):
     Name : str = Field(..., examples=["Robin hut"])
-    PhoneNumber : str = Field(...,min_length=10, max_length=10, examples=[6294123456])
-    Age : int = Field(...,ge=18, examples=["18"])
+    PhoneNumber : str = Field(...,min_length=10, max_length=10, examples=["6294123456"])
+    Age : int = Field(...,ge=18, examples=[17])
     Address : str | None = Field(None, examples=["Kolkata/WB"])
-    is_active : bool = Field(..., examples=["Active/deactive"])
+    is_active : bool = Field(..., examples=[True])
     
     
 class UserShow(BaseModel):
+    id : str = Field(..., examples=['54dg45f44-454f5d545f4s-d4f5f4d54d-547f47df5'])
     Name : str = Field(..., examples=["Robin hut"])
-    PhoneNumber : int = Field(..., examples=[6294123456])
-    age : int = Field(..., examples=["18+"])
+    PhoneNumber : int = Field(..., examples=["6294123456"])
+    age : int = Field(..., examples=[17])
 
 
 class AddUserAddress(BaseModel):
--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 from fastapi import FastAPI, HTTPException, status
-from app.routes.User import router as user_routes
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-
-UserDB = []
-
+class Name(BaseModel):
+    name : str = Field(..., examples=["sujoy"])
 
 @app.get("/")
 def read_root():
     return {"Message": "Backend Engineering Fundamentals"}
 
+userDB = ["sujoy", "sudip", "sanjoy"]
 
-app.include_router(user_routes)
+@app.get("/all", status_code=status.HTTP_200_OK)
+def show_all_elements():
+    if None in userDB:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='List is Empty'
+        )
+    
+    return userDB
+
+@app.post("/add", status_code=status.HTTP_201_CREATED)
+def add_value(user: Name) -> dict[str, str]:
+    if user in userDB:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"{user} already in db"
+        )
+        
+    userDB.append(user.name)
+    
+    return {"message": f"{user.name} is added to db"}
